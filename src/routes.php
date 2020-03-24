36f3b2c69677f63221f813edd4506f08d368e720
healthierjacobsen <40671387+healthierjacobsen@users.noreply.github.com>
2020-03-24 10:30:12 -0700

    Update routes.php

--- src/routes.php
+++ src/routes.php
@@ -5,7 +5,7 @@
  */
 
 Route::group([
-        'namespace' => '\KingStarter\LaravelSaml\Http\Controllers'
+        'namespace' => '\HealthierJacobsen\LaravelSaml\Http\Controllers'
     ], function () {
         Route::get('/saml/idp/metadata', 'SamlIdpController@metadata')->name('saml.idp.metadata');
     }
